49aa6dfb1226f362b15b9d9ae8c88fbd58187ed0
KimTaeO <taeo0328@gmail.com>
2024-04-23 19:59:36 +0900

    initial setting

---
+++ src/main/java/com/kimtaeo/thecommercetoyproject/TheCommerceToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kimtaeo.thecommercetoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCommerceToyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCommerceToyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kimtaeo/thecommercetoyproject/TheCommerceToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimtaeo.thecommercetoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommerceToyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
