a4eb915d6c1dba58eb13d87a76e89113fcfec846
KimTaeO <taeo0328@gmail.com>
2024-04-24 12:16:23 +0900

    create :: member sign up persistence layer

--- src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/repository/MemberRepository.java
+++ src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/repository/MemberRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.UUID;
 
 public interface MemberRepository extends CrudRepository<Member, UUID> {
+    Boolean existsByMemberId(String memberId);
+    Boolean existsByPhoneNumber(String phoneNumber);
+    Boolean existsByEmail(String email);
 }
