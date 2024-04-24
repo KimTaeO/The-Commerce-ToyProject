e04f6c53331cf76229b172e2274f47070457c16b
KimTaeO <taeo0328@gmail.com>
2024-04-25 08:54:10 +0900

    add :: find all with pageable method

--- src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/repository/MemberRepository.java
+++ src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/repository/MemberRepository.java
@@ -1,12 +1,16 @@
 package com.kimtaeo.thecommercetoyproject.domain.member.repository;
 
 import com.kimtaeo.thecommercetoyproject.domain.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MemberRepository extends CrudRepository<Member, UUID> {
     Boolean existsByMemberId(String memberId);
     Boolean existsByPhoneNumber(String phoneNumber);
     Boolean existsByEmail(String email);
+    Page<Member> findAll(Pageable pageable);
 }
