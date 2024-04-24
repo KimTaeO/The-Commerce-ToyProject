30e4779bd32ec7af68eef525697cd292c55ed46c
KimTaeO <taeo0328@gmail.com>
2024-04-25 08:55:00 +0900

    add :: paged members service

--- src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/service/MemberService.java
+++ src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.kimtaeo.thecommercetoyproject.domain.member.service;
 
+import com.kimtaeo.thecommercetoyproject.domain.member.enums.SortType;
 import com.kimtaeo.thecommercetoyproject.domain.member.presentation.dto.request.SignUpRequest;
+import com.kimtaeo.thecommercetoyproject.domain.member.presentation.dto.response.PagedMembersResponse;
 
 public interface MemberService {
     void signUp(SignUpRequest signUpRequest);
+    PagedMembersResponse queryMembers(Integer page, Integer pageSize, SortType sortType);
 }
--- src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/service/impl/MemberServiceImpl.java
+++ src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/service/impl/MemberServiceImpl.java
@@ -1,12 +1,19 @@
 package com.kimtaeo.thecommercetoyproject.domain.member.service.impl;
 
 import com.kimtaeo.thecommercetoyproject.domain.member.entity.Member;
+import com.kimtaeo.thecommercetoyproject.domain.member.enums.SortType;
 import com.kimtaeo.thecommercetoyproject.domain.member.exception.AlreadyExistEmailException;
 import com.kimtaeo.thecommercetoyproject.domain.member.exception.AlreadyExistPhoneNumberException;
 import com.kimtaeo.thecommercetoyproject.domain.member.presentation.dto.request.SignUpRequest;
+import com.kimtaeo.thecommercetoyproject.domain.member.presentation.dto.response.MemberResponse;
+import com.kimtaeo.thecommercetoyproject.domain.member.presentation.dto.response.PagedMembersResponse;
 import com.kimtaeo.thecommercetoyproject.domain.member.repository.MemberRepository;
 import com.kimtaeo.thecommercetoyproject.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -39,5 +46,27 @@ public class MemberServiceImpl implements MemberService {
                 .build();
 
         memberRepository.save(member);
+    }
+
+    @Override
+    public PagedMembersResponse queryMembers(Integer page, Integer pageSize, SortType sortType) {
+        Sort sort = Sort.by(Sort.Order.asc(sortType.getStr()));
+
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+
+        Page<Member> pagedMembers = memberRepository.findAll(pageable);
+
+        Page<MemberResponse> pagedResponse = pagedMembers.map(
+                member -> MemberResponse.builder()
+                        .id(member.getId())
+                        .memberId(member.getMemberId())
+                        .email(member.getEmail())
+                        .password(member.getPassword())
+                        .name(member.getName())
+                        .phoneNumber(member.getPhoneNumber())
+                        .build()
+        );
+
+        return new PagedMembersResponse(pagedResponse);
     }
 }
