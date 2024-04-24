e827cb3b50bf0b8685907ad746dee54bda76eef2
KimTaeO <taeo0328@gmail.com>
2024-04-24 12:17:09 +0900

    add :: member entity builder annotation

--- src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/entity/Member.java
+++ src/main/java/com/kimtaeo/thecommercetoyproject/domain/member/entity/Member.java
@@ -1,5 +1,7 @@
 package com.kimtaeo.thecommercetoyproject.domain.member.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +12,9 @@ import java.util.UUID;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Member {
     @Id
     @Column(columnDefinition = "BINARY(16)", nullable = false)
