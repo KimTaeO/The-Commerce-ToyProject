a73e6a74cc16a24ffb05e2a0e25ce72237bb644b
KimTaeO <taeo0328@gmail.com>
2024-04-26 19:22:57 +0900

    add :: update member web layer test with restdocs

--- src/test/java/com/kimtaeo/thecommercetoyproject/domain/member/presentation/MemberControllerTest.java
+++ src/test/java/com/kimtaeo/thecommercetoyproject/domain/member/presentation/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.kimtaeo.thecommercetoyproject.domain.member.presentation;
 
+import com.kimtaeo.thecommercetoyproject.domain.member.entity.Member;
 import com.kimtaeo.thecommercetoyproject.domain.member.presentation.dto.response.MemberResponse;
 import com.kimtaeo.thecommercetoyproject.domain.member.presentation.dto.response.PagedMembersResponse;
 import com.kimtaeo.thecommercetoyproject.domain.member.service.QueryMembersService;
@@ -24,8 +25,7 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureRestDocs
@@ -93,6 +93,54 @@ public class MemberControllerTest {
                 )
                 .andDo(MockMvcResultHandlers.print())
                 .andDo(MockMvcRestDocumentation.document("member/queryMembers",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()))
+                )
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("updateMember web layer 테스트 코드입니다.")
+    @Test
+    void updateMemberTest() throws Exception {
+        UUID id = UUID.randomUUID();
+        String memberId = "asdf1234";
+
+        Member member = Member.builder()
+                .id(id)
+                .memberId(memberId)
+                .email("asdf1234@gmail.com")
+                .password("qwer1234!")
+                .nickname("나는야홍길동")
+                .name("홍길동")
+                .phoneNumber("01012345678")
+                .build();
+
+        MemberResponse memberResponse = MemberResponse.builder()
+                .id(id)
+                .memberId(memberId)
+                .email("updatedEmail@gmail.com")
+                .password("updatedPassword1234!")
+                .nickname("updatedNickname")
+                .name("updatedName")
+                .phoneNumber("01011111111")
+                .build();
+
+        String requestBody =
+                "{\n" +
+                "    \"email\": \"updatedEmail@gmail.com\",\n" +
+                "    \"password\": \"updated1234!\",\n" +
+                "    \"nickname\": \"updatedNickname\",\n" +
+                "    \"name\": \"updatedName\",\n" +
+                "    \"phoneNumber\": \"01011111111\"\n" +
+                "}";
+
+        given(updateMemberService.updateMember(any(), any())).willReturn(memberResponse);
+
+        mockMvc.perform(patch("/api/user/{member_id}", memberId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(MockMvcRestDocumentation.document("member/updateMember",
                         preprocessRequest(prettyPrint()),
                         preprocessResponse(prettyPrint()))
                 )
